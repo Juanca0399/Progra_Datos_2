7c3ef19b3cab527ce270ade5c4645fb32b597d15
Juanca0399 <juancaruizs@github.com>
2017-11-03 23:29:02 -0600

    ABB y archivos de prueba agregados

--- nodosDeArboles.cpp
+++ nodosDeArboles.cpp
@@ -1,40 +1,170 @@
 #include <iostream>
 #include <string>
-
+#include <fstream>
+#include <sstream>
 using namespace std;
 
 class NodoBinario {
    public:
-    NodoBinario(int num, NodoBinario *der = NULL, NodoBinario *izq = NULL, NodoBinario *sig=NULL, NodoBinario *ant=NULL):
-        Hizq(izq), Hder(der), valor(num), siguiente(sig), anterior(ant), FB(0) {}
 
-    int valor;
-    int FB;
-    NodoBinario *Hizq, *Hder, *siguiente, *anterior;
+   int valor;
+   string str;
+   int FB;
+   NodoBinario *Hizq, *Hder, *siguiente, *anterior;
 
-    void InsertaBinario(int num);
+
+    NodoBinario(int llave, string str, NodoBinario *der = NULL, NodoBinario *izq = NULL, NodoBinario *sig=NULL, NodoBinario *ant=NULL):
+        Hizq(izq), Hder(der), valor(llave),str(str),siguiente(sig), anterior(ant), FB(0) {}
+
+    void InsertaBinario(int num,string cliente);
 };
 
-typedef NodoBinario *pnodo;
 typedef NodoBinario *pNodoBinario;
 
-void NodoBinario::InsertarBinario(int num){
-  if (num<valor){
-    if (Hizq == NULL){
-      Hizq = new NodoBinario(num);
+class Binario{
+public:
+    pNodoBinario raiz;
+
+    Binario():raiz(NULL){}
+
+    void InsertaNodo(string str);
+    void PreordenI();
+    void InordenI();
+    void PostordenI();
+
+    bool Hh;
+
+    void Borrar(NodoBinario *nodoB);
+    void BorrarBalanceado(NodoBinario *r, int eliminar);
+    void insertarIndices();
+    void indexar();
+    int insertarCliente();
+    void limpiar();
+    string EliminarCliente (int cedula);
+    int Buscar(NodoBinario *raiz, int llave, string str);
+
+};
+
+void NodoBinario::InsertaBinario(int llave, string str)
+{
+    if(llave<valor){
+        if(Hizq==NULL){
+            Hizq = new NodoBinario(llave,str);
+        }else{
+            Hizq->InsertaBinario(llave,str);
+        }
+    }else{
+        if(Hder==NULL){
+            Hder = new NodoBinario(llave,str);
+        }else{
+            Hder->InsertaBinario(llave,str);
+        }
     }
-    else{
-      Hizq ->InsertaBinario(num);
-    }
-  }
-  else{
-    if (Hder == NULL){
-      Hder = new NodoBinario(num);
-    }
-    else{
-      Hder->InsertaBinario(num)
-    }
+}
+
+void Binario::InsertaNodo(string str)
+{
+  size_t last_index = str.find_last_not_of("0123456789");
+  string llaveStr = str.substr(last_index + 1);
+  int llave = stoi(llaveStr);
+  if(raiz==NULL){
+    raiz = new NodoBinario(llave,str);
+  }else{
+    raiz->InsertaBinario(llave,str);
   }
 }
 
-void NodoBinario
+void PreordenR(NodoBinario *R){
+
+    if(R==NULL){
+        return;
+    }else{
+        cout<<R->str<<" - ";
+        PreordenR(R->Hizq);
+        PreordenR(R->Hder);
+    }
+}
+
+void InordenR(NodoBinario *R){
+
+    if(R==NULL){
+        return;
+    }else{
+        InordenR(R->Hizq);
+        cout<<R->valor<<","<<R->str<<" - ";
+        InordenR(R->Hder);
+    }
+}
+
+void PostordenR(NodoBinario *R){
+
+    if(R==NULL){
+        return;
+    }else{
+        PostordenR(R->Hizq);
+        PostordenR(R->Hder);
+        cout<<R->valor<<" - ";
+    }
+}
+
+string getSegmento(string str, int pos){
+  string segmento;
+  int cont = 0;
+  stringstream stream(str);
+  string resultado;
+  while (getline(stream, segmento, ';'))
+  {
+    if (cont == pos){
+      resultado = segmento;
+    }
+    cont++;
+  }
+  return resultado;
+}
+
+int Binario::Buscar(NodoBinario *raiz, int llave, string str){
+  int indice;
+  if (raiz != NULL){
+    if (llave < raiz->valor){
+      Buscar(raiz->Hizq, llave, str);
+    }
+    else{
+      if (llave > raiz->valor){
+        Buscar(raiz->Hder, llave, str);
+      }
+      else{
+        if (str == raiz->str)
+          return true;
+        else{
+          if (str == raiz->Hizq->str)
+            return true;
+          else if (str == raiz->Hder->str)
+            return true;
+          else  return false;
+        }
+      }
+    }
+  }
+  else{
+    cout << "El nodo buscado no está en el árbol" << endl;
+    return 0;
+  }
+}
+
+int main(){
+  Binario Arbol;
+  Arbol.InsertaNodo("A08;Agencia8");
+  Arbol.InsertaNodo("A03;Agencia3");
+  Arbol.InsertaNodo("A12;Agencia12");
+  Arbol.InsertaNodo("A01;Agencia1");
+  Arbol.InsertaNodo("A6;Agencia6");
+  Arbol.InsertaNodo("A14;Agencia14");
+  Arbol.InsertaNodo("A13;Agencia13");
+  PreordenR(Arbol.raiz);
+  
+  if (Arbol.Buscar(Arbol.raiz, 3,"A03;Agencia3"))
+    cout << "pene" << endl;
+    
+  cin.get();
+  return 0;
+}
